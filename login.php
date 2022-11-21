f1d845aa231725aa3e2181f5aa70d75d6cb51f0e
Julien <julien.dousse@gmail.com>
2022-11-21 16:13:24 +0100

    update login.php

--- login.php
+++ login.php
@@ -1,54 +1,38 @@
 <?php
-session_start();
-if (isset($_SESSION["user"])) {
-    header("Location: index.php");
-    exit;
-}
 
-$message = "Le mail ou le mot de passe est incorrecte";
+require_once 'partials/_check_is_not_logged.php';
+require_once 'models/User.php';
 
-if (!empty($_POST)) {
-    if (
-        isset($_POST['email'], $_POST['password'])
-        && !empty($_POST['email']) && !empty($_POST['password'])
-    ) {
-        $_SESSION["error"] = [];
-        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-            $_SESSION["error"][] = $message;
-        }
-
-        $pass = password_hash($_POST["password"], PASSWORD_ARGON2ID);
+if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 
-        require_once "connec.php";
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-        $stmt = $pdo->prepare("SELECT * FROM `user` WHERE `email` = :email");
-        $stmt->execute([
-            'email' => $_POST['email'],
-        ]);
-        $user = $stmt->fetch();
-        if (!$user) {
-            $_SESSION["error"][] = $message;
-            $user["password"] = "";
-        }
-        //Si le mail est existant, on verifie le mot de passe
-        if (!password_verify($_POST["password"], $user["password"])) {
-            $_SESSION["error"][] = "";
-        }
-        //Si il y a une erreur enregistrée dans la session, on arrête
-        if ($_SESSION["error"] === []) {
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 
-            //on ouvre la session pour la connection au gestionnaire de tâche
-            session_start();
-            $_SESSION["user"] = [
-                "id" => $user["id"],
-                "name" => $user["name"],
-                "email" => $user["email"]
+
+
+    $user = User::findOneByEmail($email);
+
+
+    if (!$user) {
+        $_SESSION['errors'][] = "nous n'avons pas un compte avec cette adresse";
+    } else {
+        if (password_verify(htmlspecialchars($_POST['password']), $user->getPassword())) {
+            $_SESSION['user'] = [
+                'is_logged' => TRUE,
+                'email' => $user->getEmail(),
+                'id' => $user->getId()
             ];
+
             header('Location: index.php');
+            exit;
+        } else {
+            $_SESSION['errors'][] = "Le mot de passe est erroné";
         }
-        unset($pdo);
     }
+} else {
+    $_SESSION['errors'][] = "Tous les champs sont obligatoires";
 }
+
+
 require_once 'views/login.php';
